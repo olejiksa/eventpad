6d1be3053e3608c6050baaf92ba695f3c461ac8c
olejiksa <quillaur@outlook.com>
2020-03-30 14:28:27 +0300

    Service for UserDefaults

---
+++ Eventpad/UserDefaultsService.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaultsService.swift
+//  Eventpad
+//
+//  Created by Oleg Samoylov on 30.03.2020.
+//  Copyright © 2020 Oleg Samoylov. All rights reserved.
+//
+
+import Foundation
+
+final class UserDefaultsService {
+    
+    func get() -> Bool {
+        let defaults = UserDefaults.standard
+        return defaults.bool(forKey: "login")
+    }
+    
+    func set(_ value: Bool) {
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: "login")
+    }
+    
+    func clear() {
+        let defaults = UserDefaults.standard
+        let dictionary = defaults.dictionaryRepresentation()
+        dictionary.keys.forEach(defaults.removeObject)
+    }
+}
